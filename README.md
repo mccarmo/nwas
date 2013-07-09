3117d18d3f1e42ceb3c671ac207ee2b514610c99
Marcelo <mccarmo@gmail.com>
2013-07-09 01:32:05 -0300

    Signed-off-by: Marcelo <mccarmo@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-## Blank Heroku app
-
-This is a empty Heroku application template. You can add files to this application once you have cloned it.
+Node.js WebAudio Server = NWAS
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var http = require("http"),
+	express = require("express"),
+    fs = require("fs"),
+    socketio = require("socket.io");
 
-app.get('/', function(request, response) {
-  response.send('Hello World Node.js , preparado para Heroku!');
-});
+var app = express(),
+    server = http.createServer(app);
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.use(express.static(__dirname))
+
+app.get('/', function (request, response) {
+    fs.createReadStream('pages/index.html').pipe(res)
+})
+
+var io = socketio.listen(server);
+
+io.sockets.on('connection', function (socket) {
+	console.log("client connected...")
+})
+
+server.listen(8000)
+console.log("listening on port 8000")
