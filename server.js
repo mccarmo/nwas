7954fc9ba011a7dd58d42fc14720ad5c12dd76ed
Marcelo <mccarmo@gmail.com>
2013-07-09 04:12:23 -0300

    Signed-off-by: Marcelo <mccarmo@gmail.com>

--- server.js
+++ server.js
@@ -1,37 +1,40 @@
 var http = require("http"),
 	express = require("express"),
-    fs = require("fs");
- //  socketio = require("socket.io");
+    fs = require("fs"),
+    socketio = require("socket.io");
 
 var app = express(),
     server = http.createServer(app);
 
-app.use(express.static(__dirname))
-
 app.get('/', function (request, response) {
-    fs.createReadStream(__dirname + '/pages/index.html').pipe(response)
+    fs.createReadStream('./pages/index.html').pipe(response)
 })
 
 app.get('/css', function (request, response) {
-    fs.createReadStream(__dirname + '/pages/css/style.css').pipe(response)
+    fs.createReadStream('./pages/css/style.css').pipe(response)
 })
 
 app.get('/audiosockets', function (request, response) {
-    fs.createReadStream(__dirname + '/pages/js/main.js').pipe(response)
+    fs.createReadStream('./pages/js/main.js').pipe(response)
 })
 
 app.get('/jquery', function (request, response) {
-    fs.createReadStream(__dirname + '/pages/js/jquery-1.10.2.min.js').pipe(response)
+    fs.createReadStream('./pages/js/jquery-1.10.2.min.js').pipe(response)
 })
 
-/*var io = socketio.listen(server);
+var io = socketio.listen(server);
+
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
 
 io.sockets.on('connection', function (socket) {
 	console.log("socket connected...")
 	socket.on("msg",function(msg){
 		console.log(msg)
 	});
-})*/
+})
 
 server.listen(8080)
 console.log("listening on port 8080...")
